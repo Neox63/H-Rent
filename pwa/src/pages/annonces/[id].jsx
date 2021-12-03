843662378106eb1bb5355d03587d34ed31b42553
Neox63 <mathischambon85@gmail.com>
2021-12-03 16:34:26 +0100

    Add .gitignore + new components

--- pwa/src/pages/annonces/[id].jsx
+++ pwa/src/pages/annonces/[id].jsx
@@ -2,7 +2,12 @@ import { useParams } from "react-router";
 
 const Annonce = () => {
   const { id } = useParams();
-  return <div>Annonce {id}</div>;
+
+  return (
+    <>
+      <div>Annonce {id}</div>
+    </>
+  );
 };
 
 export default Annonce;
