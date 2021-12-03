0578ac62f5a36ff1e086ac1d352730c92ee3f9e1
Neox63 <mathischambon85@gmail.com>
2021-12-03 14:43:22 +0100

    Front Init

---
+++ pwa/src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-center">H-Rent</div>;
+};
+
+export default App;
--- pwa/src/index.js
+++ pwa/src/index.js
@@ -1,5 +1,11 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
