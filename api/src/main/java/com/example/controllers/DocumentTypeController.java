028e5b4c4e1e9ce5f0a8e37225d36e35dc2b9397
Neox63 <mathischambon85@gmail.com>
2022-05-09 01:29:53 +0200

    Removed API, moved to Nero's repo

--- api/src/main/java/com/example/controllers/DocumentTypeController.java
+++
@@ -1,5 +0,0 @@
-package com.example.controllers;
-
-public class DocumentTypeController {
-
-}
--- api/src/main/java/com/example/repository/DocumentTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.model.DocumentType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DocumentTypeRepository extends JpaRepository<DocumentType,Integer> {
-}
