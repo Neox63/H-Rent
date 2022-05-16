b80083b48850e514dadd54162a4ec79561e65589
Neox63 <mathischambon85@gmail.com>
2022-05-16 23:01:23 +0200

    Search filters and some data fetching

--- pwa/src/components/Search/index.jsx
+++ pwa/src/components/Search/index.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { getAnnonces } from "../../fakeAPI";
+import useSWR from "swr";
 
 const Search = () => {
   const [title, setTitle] = useState("");
   const [country, setCountry] = useState("");
+  const [query, setQuery] = useState("");
 
   const history = useHistory();
 
@@ -24,9 +26,15 @@ const Search = () => {
     );
   };
 
+  const { data, isValidating } = useSWR(
+    query.length > 0
+      ? `https://api-adresse.data.gouv.fr/search/?q=${query}&limit=5`
+      : null
+  );
+
   return (
     <div className="flex flex-col w-2/3 px-4 py-8 mx-auto bg-white rounded-md custom-shadow">
-      <div className="flex gap-4 mb-8">
+      <div className="relative flex gap-4 mb-8">
         <input
           type="text"
           name="title"
@@ -36,15 +44,63 @@ const Search = () => {
           onChange={(e) => setTitle(e.target.value)}
           className="w-1/2 px-4 py-2 bg-gray-100 rounded-lg"
         />
+
         <input
           type="text"
           name="location"
           id="location"
           placeholder="Saisissez une ville ou un code postal"
           value={country}
-          onChange={(e) => setCountry(e.target.value)}
-          className="w-1/2 px-4 py-2 bg-gray-100 rounded-lg"
+          onChange={(e) => {
+            setCountry(e.target.value);
+            setQuery(e.target.value);
+          }}
+          className="relative w-1/2 px-4 py-2 bg-gray-100 rounded-lg"
         />
+        <ul
+          className={`absolute right-0 w-1/2 overflow-hidden bg-white rounded-md shadow-xl top-full`}
+        >
+          {isValidating ? (
+            <li className="py-4 text-2xl text-center text-red-500">
+              <i className="fa fa-circle-notch fa-spin"></i>
+            </li>
+          ) : (
+            <>
+              {data?.features.length > 0 ? (
+                <>
+                  {data?.features?.map((currCountry) => (
+                    <li
+                      onClick={() => {
+                        setQuery("");
+                        setCountry(currCountry?.properties?.label);
+                      }}
+                      className="flex flex-col px-8 py-4 cursor-pointer hover:bg-gray-200"
+                    >
+                      <span>
+                        {currCountry?.properties?.label} -{" "}
+                        {currCountry?.properties?.postcode}
+                      </span>
+                      <span className="text-sm text-gray-400">
+                        {currCountry?.properties?.context}
+                      </span>
+                    </li>
+                  ))}
+                </>
+              ) : (
+                query.length > 0 && (
+                  <li className="px-8 py-4 text-center">
+                    <span>
+                      Aucun résultat{" "}
+                      <span>
+                        pour "<span className={`font-bold text-green-700`}>{query}</span>"
+                      </span>
+                    </span>
+                  </li>
+                )
+              )}
+            </>
+          )}
+        </ul>
       </div>
 
       <div className="filters"></div>
--- pwa/src/pages/annonces/index.jsx
+++ pwa/src/pages/annonces/index.jsx
@@ -3,10 +3,19 @@ import AnnonceItem from "../../components/AnnonceItem";
 import { getAnnonces, getFavoriteAnnonces } from "../../fakeAPI";
 import BreadCrumb from "../../components/BreadCrumb";
 import Search from "../../components/Search";
+import { useRouterQuery } from "../../hooks/useRouterQuery";
+import useSWR from "swr";
+import { useLocation } from "react-router-dom";
 
 const Annonces = () => {
-  const initialData = getAnnonces();
+  const data = getAnnonces();
   const favoriteAnnonces = getFavoriteAnnonces();
+
+  const { search } = useLocation();
+
+  const { data: initialData, error } = useSWR(
+    `/annonces${search.length > 0 ? search : ""}`
+  );
 
   return (
     <>
@@ -15,18 +24,41 @@ const Annonces = () => {
       <Search />
 
       <Container className="w-full mx-auto lg:w-3/4">
-        <div className="mt-8 text-2xl font-extrabold">Liste des locations</div>
-        {initialData.map((annonceData, index) => (
-          <AnnonceItem
-            key={index}
-            initialData={{
-              ...annonceData,
-              isFavorite: favoriteAnnonces.some(
-                (annonce) => annonce.id === annonceData.id
-              ),
-            }}
-          />
-        ))}
+        <div className="mt-8 text-2xl font-extrabold">
+          Liste des locations disponibles
+        </div>
+
+        {error ? (
+          <div className="flex flex-col my-8 text-xl font-extrabold text-center text-red-500">
+            <span>
+              Oops, on dirait qu'une erreur s'est produite, veuillez réessayer plus tard.
+            </span>
+          </div>
+        ) : !initialData ? (
+          <div className="flex flex-col my-8 text-xl font-extrabold text-center">
+            <span>
+              Nous cherchons les meilleures annonces pour vous ! Laissez nous quelques
+              instants...
+            </span>
+            <i className="text-green-700 fa fa-circle-notch fa-spin"></i>
+          </div>
+        ) : initialData.length === 0 ? (
+          <div className="flex flex-col my-8 text-xl font-extrabold text-center">
+            Désolé, mais aucune annonce ne correspond à vos critères de recherche
+          </div>
+        ) : (
+          data.map((annonceData, index) => (
+            <AnnonceItem
+              key={index}
+              initialData={{
+                ...annonceData,
+                isFavorite: favoriteAnnonces.some(
+                  (annonce) => annonce.id === annonceData.id
+                ),
+              }}
+            />
+          ))
+        )}
       </Container>
     </>
   );
