efc6d7d1f6151121d39a0cc0f836c07957fc6a08
Neox63 <mathischambon85@gmail.com>
2022-05-19 23:00:51 +0200

    Images + another env conf

--- pwa/src/components/ImageGallery/index.jsx
+++ pwa/src/components/ImageGallery/index.jsx
@@ -1,15 +1,22 @@
 const ImageGallery = ({ images, withUrl }) => {
   return (
     <div className="flex w-full gap-4 mt-8">
-      {images.map((image, index) => (
-        <div key={index}>
-          <img
-            src={withUrl ? image : URL.createObjectURL(image)}
-            alt={image.name}
-            className="w-full h-32 rounded-lg"
-          />
-        </div>
-      ))}
+      {images.map(
+        (image, index) =>
+          image && (
+            <div key={index}>
+              <img
+                src={
+                  withUrl
+                    ? "http://localhost:8080/downloadFile/" + encodeURI(image)
+                    : URL.createObjectURL(image)
+                }
+                alt="Annonce"
+                className="w-full h-32 rounded-lg"
+              />
+            </div>
+          )
+      )}
     </div>
   );
 };
